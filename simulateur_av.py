d652e8c547e46654408a7d718cba912c2a119d73
Antoine Geek <antoine.berjoan+github@hotmail.fr>
2024-10-18 11:36:02 +0200

    Update simulateur_av.py

--- simulateur_av.py
+++ simulateur_av.py
@@ -18,7 +18,7 @@ st.set_page_config(
 
 
 def load_css():
-    with open("assets/style2.css") as f:
+    with open("assets/style.css") as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 load_css()
