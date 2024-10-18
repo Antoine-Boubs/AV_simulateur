11f7d6fcdde46183e66de13d44418347020d8929
Antoine Geek <antoine.berjoan+github@hotmail.fr>
2024-10-18 11:34:57 +0200

    Update simulateur_av.py

--- simulateur_av.py
+++ simulateur_av.py
@@ -16,9 +16,10 @@ st.set_page_config(
     }
 )
 
+
 def load_css():
-    with open("/Users/boubs/Downloads/VS/style2.css") as f:  
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+    with open("assets/style2.css") as f:
+st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 load_css()
 
